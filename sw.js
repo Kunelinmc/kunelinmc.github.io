ee1b6a25214fe83d3f07b2ec03a2942b6865c9ef
Kunelinmc <116436241+Kunelinmc@users.noreply.github.com>
2025-09-07 19:51:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**release 0.4.4*/
 const DB_NAME = "sw-unread-db";
 const DB_STORE = "unreadStore";
-const CACHE_NAME = "x18pKWgsj";
+const CACHE_NAME = "xfORkneIg";
 const PRECACHE_URLS = [
 	"/",
 	"/index.html",
